60162efcc3e09d18c2d86664528c6e3482d6e31d
WizardOfOgz <andyogzewalla@gmail.com>
2016-09-30 17:45:05 -0500

    Return duplicated values except where it is not safe. Fix bug which cause cache key collisions

--- lib/memoized_inflectors.rb
+++ lib/memoized_inflectors.rb
@@ -3,30 +3,43 @@ require "active_support/inflector"
 require "active_support/core_ext/string"
 
 module MemoizedInflectors
-  INFLECTOR_METHODS = [
-    :camelize, :classify, :constantize, :dasherize, :deconstantize, :demodulize, :foreign_key,
-    :humanize, :inflections, :ordinal, :ordinalize, :parameterize, :pluralize, :safe_constantize,
-    :singularize, :tableize, :titleize, :transliterate, :underscore
-  ].freeze
+
+  DUP_UNSAFE = %i[constantize safe_constantize].freeze
 
   def self.prepended(klass)
     klass.class_eval do
-      @memoized_inflectors = {}
-      INFLECTOR_METHODS.each do |inflector_name|
-        unless instance_variable_get("@#{ inflector_name }")
-          @memoized_inflectors[inflector_name] = instance_variable_set("@#{ inflector_name }", {})
+      @memoized_inflectors = {}  # Initialize inflector cache
+
+      # TODO: Change this to be lazy initialization *
+      ::MemoizedInflectors.inflector_methods.each do |inflector|
+        ivar = "@#{ inflector }"
+        unless instance_variable_get(ivar)
+          @memoized_inflectors[inflector] = instance_variable_set(ivar, {})
         end
       end
     end
   end
 
-  INFLECTOR_METHODS.each do |inflector_name|
-    define_method inflector_name do |*args|
-      memoized_inflections = self.class.instance_variable_get("@#{ inflector_name }")
-      key = [self, *args].hash
-      memoized_inflections.has_key?(key) ? memoized_inflections[key] : memoized_inflections[key] = super(*args)
+  def self.inflector_methods
+    ::ActiveSupport::Inflector.instance_methods
+  end
+
+  inflector_methods.each do |inflector|
+    define_method(inflector) do |*args|
+      memoized_inflections = self.class.instance_variable_get("@#{ inflector }")
+      key = [self, inflector, *args].hash
+
+      result =
+        if memoized_inflections.has_key?(key)
+          memoized_inflections[key]
+        else
+          memoized_inflections[key] = super(*args)
+        end
+
+      DUP_UNSAFE.include?(inflector) ? result : result.dup
     end
   end
 end
 
-String.send(:prepend, MemoizedInflectors)
+::String.send(:prepend, MemoizedInflectors)
+
--- lib/tasks/memoized_inflectors_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :memoized_inflectors do
-#   # Task goes here
-# end
